3e035c929f62c810c466e631c2374d2df9ac1c26
jannatulferdous2730 <alzahnimmeher@gmail.com>
2024-10-17 15:11:42 +0600

    -screenshot added

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A new Flutter project.
 
+![screenshot1](assets/images/screenshot1.png)
+
 ## Getting Started
 
 This project is a starting point for a Flutter application.
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,11 +18,11 @@ class HomePage extends StatelessWidget {
           children: [
 
             Container(
-              height: 300,
-              width: 300,
+              height: 280,
+              width: 240,
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                  child: Image.asset("assets/images/jannatul.jpg")),
+                borderRadius: BorderRadius.circular(70),
+                  child: Image.asset("assets/images/jannatul2.jpg")),
             ),
             Text("Jannatul Ferdous", style: TextStyle(
               fontWeight: FontWeight.bold,
@@ -36,8 +36,12 @@ class HomePage extends StatelessWidget {
 
               Navigator.push(context, MaterialPageRoute(builder: (context)=>PersonalInfo()));
             },
-              icon: Icon(Icons.person),
-              label: Text("Personal Info"),
+              icon: Icon(Icons.person, color: Color(0xFF4F956F),),
+              label: Text("Personal Info", style: TextStyle(
+                color: Color(0xFF4F956F),
+                fontWeight: FontWeight.w900,
+                fontSize: 16,
+              ),),
               style: ElevatedButton.styleFrom(
                 fixedSize: Size(200, 50), // Width: 200, Height: 50
               ),
@@ -46,8 +50,12 @@ class HomePage extends StatelessWidget {
             ElevatedButton.icon(onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context)=>Skills()));
             },
-              icon: Icon(Icons.lightbulb),
-              label: Text("Skills"),
+              icon: Icon(Icons.lightbulb, color: Color(0xFF4F956F),),
+              label: Text("Skills", style: TextStyle(
+                color: Color(0xFF4F956F),
+                fontWeight: FontWeight.w900,
+                fontSize: 16,
+              ),),
 
               style: ElevatedButton.styleFrom(
                 fixedSize: Size(200, 50), // Width: 200, Height: 50
@@ -57,8 +65,12 @@ class HomePage extends StatelessWidget {
             ElevatedButton.icon(onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context)=>Education()));
             },
-              icon: Icon(Icons.school),
-              label: Text("Education"),
+              icon: Icon(Icons.school, color: Color(0xFF4F956F),),
+              label: Text("Education", style: TextStyle(
+                color: Color(0xFF4F956F),
+                fontWeight: FontWeight.w900,
+                fontSize: 16,
+              ),),
               style: ElevatedButton.styleFrom(
                 fixedSize: Size(200, 50),
               ),
@@ -67,8 +79,12 @@ class HomePage extends StatelessWidget {
             ElevatedButton.icon(onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context)=>Experience()));
             },
-              icon: Icon(Icons.work),
-              label: Text("Experience"),
+              icon: Icon(Icons.work, color: Color(0xFF4F956F),),
+              label: Text("Experience", style: TextStyle(
+                color: Color(0xFF4F956F),
+                fontWeight: FontWeight.w900,
+                fontSize: 16,
+              ),),
 
               style: ElevatedButton.styleFrom(
                 fixedSize: Size(200, 50),
